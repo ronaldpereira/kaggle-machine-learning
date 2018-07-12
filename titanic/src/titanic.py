b6a2cdca1dcc67088b4abd4affeb008816b6cbba
Ronald <ronald.drp11@gmail.com>
2018-07-12 02:38:15 -0300

    titanic competition updated

--- titanic/src/titanic.py
+++ titanic/src/titanic.py
@@ -1,23 +1,77 @@
 #!/usr/bin/python3
 
 import pandas as pd
-import torch
 import numpy as np
-import re
-import sklearn
-import xgboost as xgb
-import seaborn as sns
-import matplotlib.pyplot as plt
+import math
+import csv
+
+from sklearn.preprocessing import scale
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
 
 import warnings
 warnings.filterwarnings('ignore')
 
-from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier,
-                              GradientBoostingClassifier, ExtraTreesClassifier)
 
-import titanicDataset # Titanic dataset loader lib
+def mountTitanicSet(setArray, type = 'train'):
+    pclassArray = setArray.loc[:, 'Pclass']
 
-titanic_dataset = titanicDataset.titanicDataset()
+    sexArray = list(map(lambda sex: 0 if str.lower(sex) ==
+                        'male' else 1, setArray.loc[:, 'Sex']))
 
-print(titanic_dataset.train.head(3))
-print(titanic_dataset.test.head(3))
+    ageArray = list(map(lambda age: age/100 if not np.isnan(age)
+                        else np.nanmean(setArray.loc[:, 'Age'] / 100), setArray.loc[:, 'Age']))
+
+    sibSpParchArray = setArray.loc[:, 'SibSp'] + setArray.loc[:, 'Parch']
+
+    fareArray = list(map(lambda fare: fare if str.lower(str(fare)) != 'nan' else 0, setArray.loc[:, 'Fare']))
+
+    hasCabinArray = list(map(lambda cabin: 1 if str.lower(
+        str(cabin)) != 'nan' else 0, setArray.loc[:, 'Cabin']))
+
+    portsArray = ['nan', 'c', 'q', 's']
+
+    embarkGroupArray = list(map(lambda port: portsArray.index(
+        str.lower(str(port))), setArray.loc[:, 'Embarked']))
+
+    if type == 'train':
+        survivedArray = setArray.loc[:, 'Survived']
+
+        trainingMatrix = np.array([pclassArray, sexArray, ageArray,
+                                   sibSpParchArray, fareArray, hasCabinArray, embarkGroupArray])
+        
+        return np.transpose(trainingMatrix), survivedArray
+
+    elif type == 'test':
+        trainingMatrix = np.array([pclassArray, sexArray, ageArray,
+                                   sibSpParchArray, fareArray, hasCabinArray, embarkGroupArray])
+
+        return np.transpose(trainingMatrix)
+
+np.set_printoptions(threshold=np.inf)
+trainSet = pd.read_csv('../input/train.csv', sep=',')
+testSet = pd.read_csv('../input/test.csv', sep=',')
+
+x_train, y_train = mountTitanicSet(trainSet, 'train')
+
+x_test = mountTitanicSet(testSet, 'test')
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y.astype('int'), train_size=0.8)
+
+logReg = LogisticRegression()
+
+logReg.fit(x_train, y_train)
+
+y_predict = logReg.predict(x_test)
+
+# print(f1_score(y_test, y_predict))
+
+csvWriter = csv.writer(open('../output/answers.csv', 'w', newline=''), delimiter=',')
+
+csvWriter.writerow(['PassengerId', 'Survived'])
+
+passengerID = 892
+for pred in y_predict:
+    csvWriter.writerow([passengerID, pred])
+    passengerID += 1
--- titanic/src/titanicDataset.py
+++
@@ -1,9 +0,0 @@
-#!/home/ronald/dev/kaggle-machine-learning/virtualenv/bin/python3
-
-import pandas as pd
-
-
-class titanicDataset:
-    def __init__(self):
-        self.train = pd.read_csv('../input/train.csv', sep=',')
-        self.test = pd.read_csv('../input/test.csv', sep=',')
